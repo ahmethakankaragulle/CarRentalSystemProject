c125f92c8295d1bba2a2a06dff9ed0930724dca3
Ahmet Hakan Karagülle <ahmet.hkn.25@hotmail.com>
2021-02-03 14:57:18 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CarManager nesnesi oluşturuyorum InMemory kullanıcak
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            //InMemory de hazır olan listedeki dataları yazdırıyorum
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id:" + car.Id + " - " + car.Description + " " + car.ModelYear + "   -->Daily Price:" + car.DailyPrice + "TL");
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+            //Yeni data ekleyip listeyi tekrar yazdırıyorum
+            carManager.Add(new Entities.Concrete.Car { Id = 7, BrandId = 4, ColorId = 5, Description = "Peugeot 508 131 beygir", ModelYear = 2020, DailyPrice = 600 });
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id:" + car.Id + " - " + car.Description + " " + car.ModelYear + "   -->Daily Price:" + car.DailyPrice + "TL");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+            //ID si 1 olan datayı güncelleyip listeyi tekrar yazdırıyorum
+            carManager.Update(new Entities.Concrete.Car { Id = 1, BrandId=1,Description= "Porche Taycan 600 beygir", ColorId=1, ModelYear=2015, DailyPrice = 2500 });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id:" + car.Id + " - " + car.Description + " " + car.ModelYear + "   -->Daily Price:" + car.DailyPrice + "TL");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+            //ID si 2 olan datayı siliyorum ve listeyi tekrar yazdırıyorum
+            carManager.Delete(new Entities.Concrete.Car { Id = 2 });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id:" + car.Id + " - " + car.Description + " " + car.ModelYear + "   -->Daily Price:" + car.DailyPrice + "TL");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+            //ID ye göre veri getirtiyorum
+            foreach (var car in carManager.GetById(3))
+            {
+                Console.WriteLine("Id:" + car.Id + " - " + car.Description + " " + car.ModelYear + "   -->Daily Price:" + car.DailyPrice + "TL");
+            }
+
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=1500, Description="Porche Taycan 600 beygir", ModelYear=2015},
+                new Car{Id=2, BrandId=1, ColorId=2, DailyPrice=1000, Description="Porche Cayenne 300 beygir", ModelYear=2014},
+                new Car{Id=3, BrandId=2, ColorId=2, DailyPrice=800, Description="Wosvagen Passat 150 beygir", ModelYear=2017},
+                new Car{Id=4, BrandId=2, ColorId=3, DailyPrice=1500, Description="Wosvagen Polo 95 beygir ", ModelYear=2018},
+                new Car{Id=5, BrandId=3, ColorId=4, DailyPrice=1500, Description="Mercedes C180 100 beygir", ModelYear=2016},
+                new Car{Id=6, BrandId=3, ColorId=4, DailyPrice=1500, Description="Mercede CLA200 156 beygir", ModelYear=2019}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car CarToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(CarToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car CarToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            
+            CarToUpdate.BrandId = car.BrandId;
+            CarToUpdate.ColorId = car.ColorId;
+            CarToUpdate.DailyPrice = car.DailyPrice;
+            CarToUpdate.Description = car.Description;
+            CarToUpdate.ModelYear = car.ModelYear;
+
+        }
+    }
+}
